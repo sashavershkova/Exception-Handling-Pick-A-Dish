cba4a92ee5c07ff0509b558d5f3a4efa6909ad41
sashavershkova <a.m.vershkova@gmail.com>
2025-03-19 11:26:07 -0700

    Fixed 0 input

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def select_dish(foods, selected_food):
-    if selected_food <= 0:
+    if selected_food < 0:
         raise IndexError
     print(f"Ah, {foods[selected_food]}! An excellent choice!")
 
@@ -12,13 +12,9 @@ def your_menu(foods):
     
         selected_choice = int(input("Your order number? "))
         select_dish(foods, selected_choice - 1)
-    except IndexError as error:
+    except (IndexError, ValueError) as error:
         print(f"{error} was entered.")
         print("Next time try entering something on the menu!")
-        your_menu(foods)
-    except ValueError as error:
-        print(f"{error} was entered.")
-        print("Next time try entering something between 1 and 5!")
         your_menu(foods)
 
 
